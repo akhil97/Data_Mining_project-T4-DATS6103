ae8c8a6f0a2411d4eed2aecfd28d0967fb8e787a
Akhil Bharadwaj <akhilm97@gmail.com>
2022-11-29 16:32:54 -0500

    Update heart_disease_prediction.py

--- heart_disease_prediction.py
+++ heart_disease_prediction.py
@@ -1,9 +1,42 @@
 #%%
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
- #%%
+ #%% Reading the dataset
 df = pd.read_csv('heart_disease_health_indicators_BRFSS2015.csv')
-print(df.head())
-print(df.info())
-# %%
+print("First 5 rows:", df.head())
+print("Information about dataset:", df.info())
+print("Columns of the dataset:", df.columns.tolist())
+print("Number of rows in dataset:", len(df))
+
+#%% Checking for any null values
+df.isnull().sum()
+
+
+# %% Does age have an effect on heart disease?
+print(df['HeartDiseaseorAttack'].unique().tolist())
+print("Minimum age:", min(df['Age']))
+print("Maximum age:", max(df['Age']))
+
+#Fraction of values 
+print("Percentage of people with no heart disease: ", (len(df[df['HeartDiseaseorAttack'] == 0])/len(df))*100)
+print("Percentage of people with a heart disease: ",(len(df[df['HeartDiseaseorAttack'] == 1])/len(df))*100)
+
+# Distribution of Age
+fig, axes = plt.subplots(2, figsize=(20, 30))
+sns.histplot(ax = axes[0] ,data = df, x='Age')
+plt.xlabel("Age (in years)")
+plt.ylabel("Count")
+
+# Barplot of Age vs Heart Disease
+sns.barplot(ax = axes[1], x = 'HeartDiseaseorAttack', y = 'Age', data = df)
+plt.title("Age vs Heart Disease")
+plt.xlabel("Heart Disease")
+plt.ylabel("Age (in years)")
+
+
+
+#%% Does having high BP have an effect on heart disease?
+
