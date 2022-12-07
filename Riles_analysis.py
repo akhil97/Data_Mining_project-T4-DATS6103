3316cbf247b7aaf09992158dd9ae1b5ea95e73c0
dustinriles <112441128+dustinriles@users.noreply.github.com>
2022-12-06 19:28:34 -0500

    Update Riles_analysis.py
    Added basic models for impact of high cholesterol and physical activity on heart disease or attack

--- Riles_analysis.py
+++ Riles_analysis.py
@@ -2,10 +2,13 @@
 import pandas as pd
 from statsmodels.formula.api import ols
 #%%
-data = pd.read_csv(r"C:\Users\riles\Desktop\heart_disease_health_indicators_BRFSS2015.csv")
-data.head(5)
+heart_data = pd.read_csv(r"C:\Users\riles\Desktop\heart_disease_health_indicators_BRFSS2015.csv")
+heart_data.head(5)
 # %%
-phyact_model = ols(formula='HeartDiseaseorAttack ~ PhysActivity', data=data)
+phyact_model = ols(formula='HeartDiseaseorAttack ~ PhysActivity', data=heart_data)
 phyact_model_fit = phyact_model.fit()
 print(phyact_model_fit.summary())
 # %%
+highchol_model = ols(formula='HeartDiseaseorAttack ~ CholCheck', data=heart_data)
+highchol_model_fit = highchol_model.fit()
+print(highchol_model_fit.summary())
