a27bf8f452eecd174eabe3c4ecfe74cc3223abdf
Sudhanshu Deshpande <sudhanshud98@gwu.edu>
2022-12-12 15:35:59 -0500

    test_update

--- EDA.py
+++ EDA.py
@@ -62,6 +62,9 @@ plt.ylabel("Age (in years)")
 print("Percentage of people with no heart disease: ", (len(df[df['HeartDiseaseorAttack'] == 0])/len(df))*100)
 print("Percentage of people with a heart disease: ",(len(df[df['HeartDiseaseorAttack'] == 1])/len(df))*100)
 
+# ttest of means of age for having heart disease or not
+rp.ttest(group1= df["Age"][df["HeartDiseaseorAttack"] == 0], group1_name= "0",
+        group2= df["Age"][df["HeartDiseaseorAttack"] == 1], group2_name= "1")
 
 #%% Does having high BP have an effect on heart disease?
 print(df['HighBP'].unique().tolist())
@@ -79,6 +82,10 @@ print(my_crosstab)
 # Catplot for HighBP vs Heart Disease
 sns.catplot(x='HighBP', hue='HeartDiseaseorAttack', data=df, kind='count', palette = "rocket")
 plt.title("HighBP vs Heart Disease")
+
+# ttest of heart disease for having high blood pressure or not
+rp.ttest(group1= df["HeartDiseaseorAttack"][df["HighBP"] == 0], group1_name= "0",
+        group2= df["HeartDiseaseorAttack"][df["HighBP"] == 1], group2_name= "1")
 
 # %% Does heavy alcohol consumption cause heart attack or disease?
 print("The unique values under Heavy Alcohol Consumption are:", df['HvyAlcoholConsump'].unique().tolist())
@@ -103,6 +110,10 @@ sns.catplot(x = "HvyAlcoholConsump",
             data = df,
             kind = "count", palette = "rocket")
 plt.title("HvyAlcoholConsump vs HeartDiseaseorAttack")
+
+# ttest of heart disease for different levels of alcohol consumption
+rp.ttest(group1= df["HeartDiseaseorAttack"][df["HvyAlcoholConsump"] == 0], group1_name= "0",
+        group2= df["HeartDiseaseorAttack"][df["HvyAlcoholConsump"] == 1], group2_name= "1")
 
 # From all the above information we can see that people who consume alcohol heavily and have a heart attack are only 848 through out the dataset which clearly indicates that the dataset is imbalanced.
 
@@ -149,6 +160,10 @@ sns.catplot(x = "Income",
             kind = "count", palette = "rocket")
 plt.title("Income vs HeartDiseaseorAttack")
 
+# ttest of income level for having heart disease or not
+rp.ttest(group1= df["Income"][df["HeartDiseaseorAttack"] == 0], group1_name= "0",
+        group2= df["Income"][df["HeartDiseaseorAttack"] == 1], group2_name= "1")
+
 # Checking to see if Heavy alcohol consumption has an impact on income
 sns.countplot(data = df, 
               x = "HvyAlcoholConsump", 
@@ -173,6 +188,10 @@ fig = px.histogram(df, x="BMI",
 fig.update_layout(yaxis_range=[-1000,25000])
 fig.show()
 
+# ttest of BMI for having heart disease or not
+rp.ttest(group1= df["BMI"][df["HeartDiseaseorAttack"] == 0], group1_name= "0",
+        group2= df["BMI"][df["HeartDiseaseorAttack"] == 1], group2_name= "1")
+
 #How consuming veggies and fruits can affect the heart condition?
 #Fruits :-
 fruits_cons = pd.crosstab(index = df["HeartDiseaseorAttack"],
@@ -187,6 +206,9 @@ print("Percentage of people who do eat fruits: ",(len(df[df['Fruits'] == 1])/len
 sns.catplot(x='Fruits', hue='HeartDiseaseorAttack', data=df, kind='count', palette = "rocket")
 plt.title("Consumption of Fruits vs Heart Disease")
 
+# ttest of heart disease for eating fruits or not
+rp.ttest(group1= df["HeartDiseaseorAttack"][df["Fruits"] == 0], group1_name= "0",
+        group2= df["HeartDiseaseorAttack"][df["Fruits"] == 1], group2_name= "1")
 #Veggies :-
 veggies_cons = pd.crosstab(index = df["HeartDiseaseorAttack"],
                             columns = df["Veggies"],
@@ -199,6 +221,10 @@ print("Percentage of people who do eat veggies: ",(len(df[df['Veggies'] == 1])/l
 #Histogram for Veggies vs HeartDiseaseorAttack
 sns.catplot(x='Veggies', hue='HeartDiseaseorAttack', data=df, kind='count', palette = "rocket")
 plt.title("Consumption of Veggies vs Heart Disease")
+
+# ttest of heart disease for eating vegetables or not
+rp.ttest(group1= df["HeartDiseaseorAttack"][df["Veggies"] == 0], group1_name= "0",
+        group2= df["HeartDiseaseorAttack"][df["Veggies"] == 1], group2_name= "1")
 
 #Histogram to check how the consumption of Fruits and Veggies affect the BMI :-
 fig = go.Figure()
@@ -228,41 +254,43 @@ fig.show()
 # %%
 #EDA for physical activity
 
-
 # Plot of counts for physical activity
-df["PhysActivity"].value_counts().plot(kind= "bar")
+df["PhysActivity"].value_counts().plot(kind= "bar", cmap="viridis")
 plt.xlabel("Physical Activity")
 plt.ylabel("Count")
 plt.title("Counts of Physical Activity")
 
-# %%
 # Plot of Physical Activity and Heart Disease
-sns.catplot(x = "PhysActivity", hue="HeartDiseaseorAttack", data=df, kind="count")
+sns.catplot(x = "PhysActivity", hue="HeartDiseaseorAttack", data=df, kind="count", palette = "rocket")
 plt.xlabel("Physical Activity")
 plt.ylabel("Count")
 plt.title("Heart Disease versus Physical Activity")
 
+# ttest of heart disease for different levels of physical activity
+rp.ttest(group1= df["HeartDiseaseorAttack"][df["PhysActivity"] == 0], group1_name= "0",
+        group2= df["HeartDiseaseorAttack"][df["PhysActivity"] == 1], group2_name= "1")
 
-# %%
 #EDA for high cholesterol
 
 # Plot of counts for high cholesterol
-df["HighChol"].value_counts().plot(kind= "bar")
+df["HighChol"].value_counts().plot(kind= "bar", cmap="viridis")
 plt.xlabel("High Cholesterol")
 plt.ylabel("Count")
 plt.title("Counts of High Cholesterol")
 
-# %%
 # Plot of High Cholesterol and Heart Disease
-sns.catplot(x = "HighChol", hue="HeartDiseaseorAttack", data=df, kind="count")
+sns.catplot(x = "HighChol", hue="HeartDiseaseorAttack", data=df, kind="count", palette = "rocket")
 plt.xlabel("High Cholesterol")
 plt.ylabel("Count")
 plt.title("Heart Disease versus High Cholesterol")
 
+# ttest of heart disease for having high cholesterol or not
+rp.ttest(group1= df["HeartDiseaseorAttack"][df["HighChol"] == 0], group1_name= "0",
+        group2= df["HeartDiseaseorAttack"][df["HighChol"] == 1], group2_name= "1")
 
 #%%
 #Smoker vs HeartDiseaseorAttack
-sns.catplot(x='Smoker', hue='HeartDiseaseorAttack', data=df, kind='count')
+sns.catplot(x='Smoker', hue='HeartDiseaseorAttack', data=df, kind='count', palette = "rocket")
 plt.title("Smoking vs Heart Disease")
 
 #As we can see here the person who dont smoke have good chance of not getting any heart disease but for the person who smokes graph shows the chance of not getting 
@@ -270,45 +298,38 @@ plt.title("Smoking vs Heart Disease")
 
 #%%
 #Smoker vs Sex
-sns.countplot(x='Sex', data=df, hue='Smoker')
+sns.countplot(x='Sex', data=df, hue='Smoker', palette = "rocket")
 
 #Here we can see that the sex=0 have more number of smokers and less number of non smoker whereas the sex=1 have equal number of smokers and non smokers. 
 # Also the smokers are more for sex=1 than sex=0.
 
+# ttest of heart disease for being a smoker or not
+rp.ttest(group1= df["HeartDiseaseorAttack"][df["Smoker"] == 0], group1_name= "0",
+        group2= df["HeartDiseaseorAttack"][df["Smoker"] == 1], group2_name= "1")
+        
 #So now we are checking the gender wise chance of getting heart disease.
 
 #%%
 #Sex vs HeartDiseaseorAttack
-sns.countplot(x='Sex',data=df, hue='HeartDiseaseorAttack')
+sns.countplot(x='Sex',data=df, hue='HeartDiseaseorAttack', palette = "rocket")
 
 #As we saw in previous plot that the sex=1 have more number of smokers, and according to the current plot we can see the chance of getting heart disease for sex=1 is more than that of sex=0. 
-# So we can say that smoking can increase of getting heart disease which we can clearly see in below plot.
 
-#Smoker vs HeartDiseaseorAttack
-sns.countplot(x='Smoker',data=df, hue='HeartDiseaseorAttack')
 
 #%%
 #Now lets see plot between Diabetes and HeartDiseaseorAttack
 #Diabetes vs HeartDiseaseorAttack
-sns.catplot(x='Diabetes', hue='HeartDiseaseorAttack', data=df, kind='count')
+sns.catplot(x='Diabetes', hue='HeartDiseaseorAttack', data=df, kind='count', palette = "rocket")
 plt.title("Diabetes vs Heart Disease")
 
-#%%
-#Next lets see the plot between Physical Activities vs HeartDiseaseorAttack
-#PhysActivity vs HeartDiseaseorAttack
-sns.countplot(x='PhysActivity', data=df, hue='HeartDiseaseorAttack')
-
-#As plot shows, person involved in any type of physical activity have better chance of not getting any hear disease than the person who is not doing any physical activity. 
-# But it also increase the chance of getting heart disease compared to the person who is not doing any physical activity.
+# ttest of heart disease for having diabetes or not
+rp.ttest(group1= df["HeartDiseaseorAttack"][df["Diabetes"] == 0], group1_name= "0",
+        group2= df["HeartDiseaseorAttack"][df["Diabetes"] == 2], group2_name= "1")
 
 #%%
-#Plot for all the variables vs HeartDiseaseorAttack
+#Plot for all the variables vs HeartDiseaseorAttack along with the ttest.
 
-catcol = ['HighBP', 'HighChol', 'CholCheck',
-       'Smoker', 'Stroke', 'Diabetes', 'PhysActivity', 'Fruits', 'Veggies',
-       'HvyAlcoholConsump', 'AnyHealthcare', 'NoDocbcCost', 'GenHlth',
-       'DiffWalk', 'Sex', 'Education',
-       'Income']
+catcol = ['CholCheck', 'Stroke', 'AnyHealthcare', 'NoDocbcCost', 'GenHlth', 'MentHlth', 'PhysHlth', 'DiffWalk', 'Sex', 'Education']
 
 plt.figure(figsize=(15,50))
 for i,column in enumerate(catcol[1:]):
@@ -318,58 +339,15 @@ for i,column in enumerate(catcol[1:]):
     plt.title(f"{column}")
     plt.tight_layout()
 
-
-# T-TESTS
-
-# %%
-# ttest of heart disease for having high blood pressure or not
-rp.ttest(group1= df["HeartDiseaseorAttack"][df["HighBP"] == 0], group1_name= "0",
-        group2= df["HeartDiseaseorAttack"][df["HighBP"] == 1], group2_name= "1")
-
-# %%
-# ttest of heart disease for having high cholesterol or not
-rp.ttest(group1= df["HeartDiseaseorAttack"][df["HighChol"] == 0], group1_name= "0",
-        group2= df["HeartDiseaseorAttack"][df["HighChol"] == 1], group2_name= "1")
-
 # %%
 # ttest of heart disease for different levels of CholCheck
 rp.ttest(group1= df["HeartDiseaseorAttack"][df["CholCheck"] == 0], group1_name= "0",
         group2= df["HeartDiseaseorAttack"][df["CholCheck"] == 1], group2_name= "1")
 
 # %%
-# ttest of BMI for having heart disease or not
-rp.ttest(group1= df["BMI"][df["HeartDiseaseorAttack"] == 0], group1_name= "0",
-        group2= df["BMI"][df["HeartDiseaseorAttack"] == 1], group2_name= "1")
-
-# %%
-# ttest of heart disease for being a smoker or not
-rp.ttest(group1= df["HeartDiseaseorAttack"][df["Smoker"] == 0], group1_name= "0",
-        group2= df["HeartDiseaseorAttack"][df["Smoker"] == 1], group2_name= "1")
-
-# %%
 # ttest of heart disease for having a stroke or not
 rp.ttest(group1= df["HeartDiseaseorAttack"][df["Stroke"] == 0], group1_name= "0",
         group2= df["HeartDiseaseorAttack"][df["Stroke"] == 1], group2_name= "1")
-
-# %%
-# ttest of heart disease for having diabetes or not
-rp.ttest(group1= df["HeartDiseaseorAttack"][df["Diabetes"] == 0], group1_name= "0",
-        group2= df["HeartDiseaseorAttack"][df["Diabetes"] == 2], group2_name= "1")
-
-# %%
-# ttest of heart disease for different levels of physical activity
-rp.ttest(group1= df["HeartDiseaseorAttack"][df["PhysActivity"] == 0], group1_name= "0",
-        group2= df["HeartDiseaseorAttack"][df["PhysActivity"] == 1], group2_name= "1")
-
-# %%
-# ttest of heart disease for eating vegetables or not
-rp.ttest(group1= df["HeartDiseaseorAttack"][df["Veggies"] == 0], group1_name= "0",
-        group2= df["HeartDiseaseorAttack"][df["Veggies"] == 1], group2_name= "1")
-
-# %%
-# ttest of heart disease for eating fruits or not
-rp.ttest(group1= df["HeartDiseaseorAttack"][df["Fruits"] == 0], group1_name= "0",
-        group2= df["HeartDiseaseorAttack"][df["Fruits"] == 1], group2_name= "1")
 
 # %%
 # ttest of heart disease for different levels of AnyHealthcare
@@ -407,21 +385,6 @@ rp.ttest(group1= df["HeartDiseaseorAttack"][df["Sex"] == 0], group1_name= "0",
         group2= df["HeartDiseaseorAttack"][df["Sex"] == 1], group2_name= "1")
 
 # %%
-# ttest of means of age for having heart disease or not
-rp.ttest(group1= df["Age"][df["HeartDiseaseorAttack"] == 0], group1_name= "0",
-        group2= df["Age"][df["HeartDiseaseorAttack"] == 1], group2_name= "1")
-
-# %%
-# ttest of heart disease for different levels of alcohol consumption
-rp.ttest(group1= df["HeartDiseaseorAttack"][df["HvyAlcoholConsump"] == 0], group1_name= "0",
-        group2= df["HeartDiseaseorAttack"][df["HvyAlcoholConsump"] == 1], group2_name= "1")
-
-# %%
 # ttest of education level for having heart disease or not
 rp.ttest(group1= df["Education"][df["HeartDiseaseorAttack"] == 0], group1_name= "0",
         group2= df["Education"][df["HeartDiseaseorAttack"] == 1], group2_name= "1")
-
-# %%
-# ttest of income level for having heart disease or not
-rp.ttest(group1= df["Income"][df["HeartDiseaseorAttack"] == 0], group1_name= "0",
-        group2= df["Income"][df["HeartDiseaseorAttack"] == 1], group2_name= "1")
